af8e6e6dac7722a5e053e7e15ed25df424b06f64
AaronCab <Aaron@Pursuits-MacBook-Pro-17.local>
2018-10-05 14:54:40 -0400

    initial ccommit of my xcode project

---
+++ Xcode-Github/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Xcode-Github
+//
+//  Created by Aaron Cabreja on 10/5/18.
+//  Copyright © 2018 Aaron Cabreja. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
